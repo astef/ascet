c0dee3585e93e0a7e90ec212705d112de40a4f06
Alexander Stefurishin <alexander.stefurishin@trilogy.com>
2022-08-29 20:26:41 +0300

    initial commit

---
+++ src/lib.ts
@@ -0,0 +1,10 @@
+export type AscetOptions = {
+  elementId: string;
+};
+
+export function init(options: AscetOptions) {
+  //     window.addEventListener("load", (ev) => {});
+  //   window.addEventListener("resize", (ev) => {
+  //   });
+  //   window.addEventListener("mousedown")
+}
